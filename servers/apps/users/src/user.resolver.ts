4ed0b4c68221a73897c59fe209564f7b2399084c
sanyama0554 <sanyama554@gmail.com>
2024-06-17 19:08:02 +0900

    Userサービスとリゾルバ作成

---
+++ servers/apps/users/src/user.resolver.ts
@@ -0,0 +1,30 @@
+import { Args, Mutation, Resolver, Query } from "@nestjs/graphql";
+import { UsersService } from "./users.service";
+import { RegisterResponse } from "./types/user.types";
+import { RegisterDto } from "./dto/user.dto";
+import { BadRequestException } from "@nestjs/common";
+import { User } from "./entities/user.entity";
+
+@Resolver('User')
+export class UsersResolver {
+  constructor (
+    private readonly userService: UsersService
+  ) {}
+
+  @Mutation(() => RegisterResponse)
+  async register(
+    @Args('registerInput') registerDto: RegisterDto,
+  ): Promise<RegisterResponse> {
+    if (!registerDto.name || !registerDto.email || !registerDto.password) {
+      throw new BadRequestException('Please fill the all fields');
+    }
+
+    const user = await this.userService.register(registerDto);
+    return { user };
+  }
+
+  @Query(() => [User])
+  async getUsers() {
+    return this.userService.getUsers();
+  }
+}
--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto, RegisterDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  // register user
+  async register(registerDto: RegisterDto) {
+    const { name, email, password } = registerDto;
+    const user = {
+      name,
+      email,
+      password
+    };
+    return user;
+  }
+
+  // Login service
+  async Login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const user = {
+      email,
+      password
+    };
+    return user;
+  }
+
+  // get all users service
+  async getUsers() {
+    const users = [
+      {
+        id: "1234",
+        name: "test",
+        email: "abc@gmail.com",
+        password: "12345678"
+      }
+    ];
+    return users;
   }
 }
